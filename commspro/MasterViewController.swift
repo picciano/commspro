721159a383611ceb044259a15d0eefa41f2aaa09
Anthony Picciano <anthony.picciano@gmail.com>
2016-11-25 11:43:23 -0700

    Navigation

--- commspro/MasterViewController.swift
+++ commspro/MasterViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
+    var splashViewController: SplashViewController? = nil
+    
+    let sections = [ "Management",
+                     "Subscribed Channels",
+                     "Topical Channels",
+                     "Geographic Channels" ]
+    
+    let management = [ "Account", "Hidden Channels", "About Comms Pro" ]
+    var subscribedChannels = [ "Alpha", "Bravo", "Charlie" ]
+    var topicalChannels = [ "Delta", "Echo", "Foxtrot" ]
+    var geographicChannels = [ "Golf", "Hotel", "India" ]
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        //
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+            splashViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? SplashViewController
+            splashViewController?.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
+            splashViewController?.navigationItem.leftItemsSupplementBackButton = true
         }
     }
 
@@ -38,19 +51,32 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        self.tableView.insertRows(at: [indexPath], with: .automatic)
+        performSegue(withIdentifier: "showHidden", sender: self)
+//        objects.insert(NSDate(), at: 0)
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        self.tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+        if segue.identifier == "showAccount" {
+//            if let indexPath = self.tableView.indexPathForSelectedRow {
+//                let object = objects[indexPath.row] as! NSDate
+//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+//            }
+        }
+        
+        if segue.identifier == "showHidden" {
+            
+        }
+        
+        if segue.identifier == "showChannel" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let controller = (segue.destination as! UINavigationController).topViewController as! ChannelViewController
+                controller.channel = item(for: indexPath)
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -60,35 +86,90 @@ class MasterViewController: UITableViewController {
     // MARK: - Table View
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        switch section {
+        case 0:
+            return management.count
+        case 1:
+            return subscribedChannels.count
+        case 2:
+            return topicalChannels.count
+        case 3:
+            return geographicChannels.count
+        default:
+            return 0
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        cell.textLabel?.text = item(for: indexPath)
         return cell
     }
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return indexPath.section == 1
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            objects.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+//            objects.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let header = view as? UITableViewHeaderFooterView {
+            header.textLabel?.textColor = .white
+            header.backgroundColor = CommsProStyleKit.commsDarkTan
+        }
+    }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 0 {
+                performSegue(withIdentifier: "showAccount", sender: self)
+            } else if indexPath.row == 1 {
+                performSegue(withIdentifier: "showHidden", sender: self)
+            } else {
+                performSegue(withIdentifier: "showSplash", sender: self)
+            }
+        case 1...3:
+            performSegue(withIdentifier: "showChannel", sender: self)
+        default:
+            break
+        }
+    }
+    
+    private func item(for indexPath: IndexPath) -> String? {
+        var item: String?
+        
+        switch indexPath.section {
+        case 0:
+            item = management[indexPath.row]
+        case 1:
+            item = subscribedChannels[indexPath.row]
+        case 2:
+            item = topicalChannels[indexPath.row]
+        case 3:
+            item = geographicChannels[indexPath.row]
+        default:
+            break
+        }
+        
+        return item
+    }
 
 }
 
